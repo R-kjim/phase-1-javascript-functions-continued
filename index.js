f2af3f72137a7e9eb4218170ee5e1e5f87c1c7ea
R-kjim <rkayjim@gmail.com>
2024-06-26 06:21:34 +0300

    creates default and wrapped functions

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+//creates a function that has a default value and can be overridden
+function saturdayFun(){
+}
+function saturdayFun(String='roller-skate'){
+    return(`This Saturday, I want to ${String}!`)
+}
+function mondayWork(string='go to the office'){
+    return(`This Monday, I will ${string}.`)
+}
+
+//creates a functionthat is wrapped in another function
+function wrapAdjective(str1){
+    return function(str2){
+        return(`You are ${str1}${str2}${str1}!`)
+    }
+}
